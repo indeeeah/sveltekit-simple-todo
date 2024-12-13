85b5fb2ccdad2aa7ea9d6c1265946aa929c4cc69
indeeeah <indeeeah@gmail.com>
2024-12-13 15:21:57 +0900

    simple todo javascript

---
+++ src/components/Header.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { AppBar, LightSwitch } from '@skeletonlabs/skeleton';
+</script>
+
+<AppBar>
+    <svelte:fragment slot="lead" >
+        <a href="/" class="font-bold">
+            simple to-do
+        </a> 
+    </svelte:fragment>
+	<svelte:fragment slot="trail"><LightSwitch /></svelte:fragment>
+</AppBar>
---
+++ src/components/Navigator.svelte
@@ -0,0 +1,42 @@
+<script>
+    import HomeIcon from '$lib/icons/HomeIcon.svelte';
+    import ListIcon from '$lib/icons/ListIcon.svelte';
+    import { today } from '@internationalized/date';
+    import { AppRail, AppRailAnchor } from '@skeletonlabs/skeleton';
+    import { page } from '$app/stores';
+
+    let now = today('Asia/Seoul');
+    let currentTile = 0;
+    let navItems = [
+        { name: "Home", href: "/", icon: HomeIcon },
+        { name: "To-do", href: "/to-do/:date", icon: ListIcon },
+    ];
+
+    function isSelected(href) {
+        if (href === "/") {
+            return $page.url.pathname === href;
+        }
+        if (href.startsWith("/to-do/:date")) {
+            return $page.url.pathname.startsWith("/to-do/");
+        }
+        return false;
+    }
+
+    const formattedNow = now.toString();
+</script>
+
+<AppRail>
+    {#each navItems as item}
+        <AppRailAnchor
+            bind:group={currentTile}
+            name={item.name}
+            title={item.name}
+            selected={isSelected(item.href)}
+            href={item.href.replace(":date", formattedNow)}>
+            <svelte:fragment slot="lead">
+                <svelte:component this={item.icon} />
+            </svelte:fragment>
+            <span>{item.name}</span>
+        </AppRailAnchor>
+    {/each}
+</AppRail>
